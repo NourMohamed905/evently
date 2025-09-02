9ff85fbd9b574786949ce2111e307cd194488a8f
NourMohamed905 <farouk.graduate2022@gmail.com>
2025-09-02 14:36:28 +0300

    add navbar and shared pref on onboarding screens

---
+++ lib/tabs/map_tab/map_tab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class mapTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('map Tab'),
+      ),
+    );
+  }
+}
